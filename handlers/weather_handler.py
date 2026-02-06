f936e3185a79559445d10c39be139326679f079f
Stepan Pomogaibo <haroshij@yandex.ru>
2026-02-06 14:45:56 +0300

    Добавлены кнопки "Отмена" и "В меню" при смене города в Погоде

--- handlers/weather_handler.py
+++ handlers/weather_handler.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext
 
 from handlers.weather import get_weather
@@ -15,8 +15,14 @@ async def weather_handler(update: Update, _: CallbackContext):
     desc = data["weather"][0]["description"]
     temp = data["main"]["temp"]
 
+    kb = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔄 Другой город", callback_data="weather_change")],
+        [InlineKeyboardButton("↩️ В меню", callback_data="menu")]
+    ])
+
     await update.message.reply_text(
         f"🌤 {city}:\n"
         f"{desc}\n"
-        f"🌡 Температура: {temp}°C"
+        f"🌡 Температура: {temp}°C",
+        reply_markup = kb
     )
