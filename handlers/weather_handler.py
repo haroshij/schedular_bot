bf86abecc62a8ddeec4f27414f10a8adad18d496
Stepan Pomogaibo <haroshij@yandex.ru>
2026-02-06 14:51:31 +0300

    Добавлено запоминание последнего выбранного города пользователя в Погоде

--- handlers/weather_handler.py
+++ handlers/weather_handler.py
@@ -2,15 +2,21 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext
 
 from handlers.weather import get_weather
+from database import set_user_city  # подключаем функцию для сохранения города
 
 
 async def weather_handler(update: Update, _: CallbackContext):
     city = update.message.text.strip()
+    user_id = update.effective_user.id
+
     data = await get_weather(city)
 
     if "error" in data:
         await update.message.reply_text(f"❌ {data['error']}")
         return
+
+    # Сохраняем город в базе после успешного получения погоды
+    await set_user_city(user_id, city)
 
     desc = data["weather"][0]["description"]
     temp = data["main"]["temp"]
@@ -24,5 +30,5 @@ async def weather_handler(update: Update, _: CallbackContext):
         f"🌤 {city}:\n"
         f"{desc}\n"
         f"🌡 Температура: {temp}°C",
-        reply_markup = kb
+        reply_markup=kb
     )
