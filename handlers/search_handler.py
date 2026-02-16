764b1b694149920e7e86255442255b20384d5a58
Stepan Pomogaibo <haroshij@yandex.ru>
2026-02-16 11:02:28 +0300

    Скорректирован текст бота после выдачи результатов запроса

--- handlers/search_handler.py
+++ handlers/search_handler.py
@@ -44,8 +44,7 @@ async def search_handler(update: Update, _: CallbackContext):
     results = await search_duckduckgo(query)
 
     # Формируем текст ответа из первых 5 результатов
-    text = """Вот, что найдено по вашему запросу.
-    Для повторного поиска отправьте новый запрос\n\n""" + "\n\n".join(results[:5])
+    text = """Для повторного поиска отправьте новый запрос\n\n""" + "\n\n".join(results[:5])
 
     # Отправляем результаты пользователю
     await update.message.reply_text(
