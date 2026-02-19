5815507d4e7f34e8cb2b748a3e6b6e2af4559e44
Stepan Pomogaibo <haroshij@yandex.ru>
2026-02-19 18:31:26 +0300

    Вместо job_queue работа с напоминаниями будет через Сelery

--- bot/jobs.py
+++ bot/jobs.py
@@ -8,11 +8,13 @@ from datetime import datetime, timezone
 from telegram.ext import CallbackContext
 
 from app.logger import logger
+from bot.tasks import send_task_reminder_task
 from database import get_task_by_id, get_all_pending_tasks
 from keyboard import task_actions
 from utils.tasks_utils import format_task
 
 
+# НЕ ИСПОЛЬЗУЕТСЯ ПРИ ПЕРЕХОДЕ НА CELERY
 async def send_task_reminder(context: CallbackContext):
     """Отправляет напоминание о задаче пользователю.
 
@@ -54,12 +56,11 @@ async def send_task_reminder(context: CallbackContext):
         logger.error("Ошибка при отправке напоминания для задачи %s\n%s", task["id"], e)
 
 
-async def restore_jobs(app):
-    """Восстанавливает все pending задачи при старте бота.
-
-    Args:
-        app (telegram.ext.Application): Экземпляр Application бота, содержит job_queue.
+async def restore_jobs(_):
     """
+    Восстанавливает все pending задачи с Celery вместо job_queue.
+    """
+
     logger.debug("Формирование напоминаний для всех невыполненных задач...")
     now = datetime.now(timezone.utc)
     tasks = await get_all_pending_tasks()
@@ -68,20 +69,8 @@ async def restore_jobs(app):
         if task["scheduled_time"] <= now:
             continue
 
-        job_name = f"task_{task['id']}"
-
-        old_jobs = app.job_queue.get_jobs_by_name(job_name)
-        for job in old_jobs:
-            job.schedule_removal()
-
         delay = (task["scheduled_time"] - now).total_seconds()
-
-        app.job_queue.run_once(
-            send_task_reminder,
-            max(0, delay),
-            data={
-                "task": task,
-                "chat_id": task["user_id"],
-            },
-            name=job_name,
+        send_task_reminder_task.apply_async(
+            args=[task["id"], task["user_id"], str(task["scheduled_time"])],
+            countdown=max(0, delay)
         )
